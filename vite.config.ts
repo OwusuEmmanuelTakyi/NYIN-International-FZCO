a7401efc5339a55575dcc52838c8995f857301fb
Owusu Emmanuel Takyi <owusuemmanueltakyi@gmail.com>
2026-04-29 22:55:06 +0000

    added the site map

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-
+import sitemap from 'vite-plugin-sitemap'
 
 function figmaAssetResolver() {
   return {
@@ -19,18 +19,26 @@ function figmaAssetResolver() {
 export default defineConfig({
   plugins: [
     figmaAssetResolver(),
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
+    sitemap({
+      hostname: 'https://www.nyinintl.com',
+      dynamicRoutes: [
+        '/',
+        '/about',
+        '/blog',
+        '/contact',
+        '/leadership',
+        '/mining',
+        '/streaming',
+        '/trading',
+      ],
+    }),
   ],
   resolve: {
     alias: {
-      // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
     },
   },
-
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
